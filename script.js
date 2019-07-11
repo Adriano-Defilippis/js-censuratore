e14fa9cb047d771e87edca71c0c05c1492f7347f
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-07-11 19:56:21 +0200

    Lo script confronta se l'array con input, presenta item contenuti nell'Array badWord, e li sostituisce con una stringa"xxx". Dopo restituisce dall'Array una stringa con il testo censurato

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // - Restituisce il testo con xxx al posto delle parole censurate.
 // - Stampa un “badword index” calcolato come il numero di parole censurate su il numero di parole totali
 
-var text, badwordList;
+var text, textTrim, badwordList;
 
 
 // Lista Parole da censurare
@@ -32,13 +32,35 @@ badwordList = [
 // Input per il controllo delle parole da censurare
 text = prompt("inserisci il testo da controllare");
 
+for (var i = 0; i < badwordList.length; i++) {
+  badwordList[i] = badwordList[i].toUpperCase();
+
+}
+
 
 console.log(badwordList);
 console.log(text);
 
 
 
-var textTrim;
 // Trasformo il testo inserito in un Array di stringhe, dove ogni stringa è una parole dell'input inserito
-textTrim = text.split(" ");
+textTrim = text.toUpperCase().split(" ");
 console.log(textTrim);
+
+
+//Ciclo all'interno dell'array con il mio testo input splittato
+for (var i = 0; i < textTrim.length; i++) {
+
+  // Se nell'array delle parole da censurare, compare un item dell'array creato dal testo spilittato inserito dall'utente,
+  // allora questo item viene sostituito con una stringa "xxx"
+  if (badwordList.includes(textTrim[i])) {
+    textTrim[i] = "xxx";
+  }
+}
+
+// Restituisco la stringa di testo inserita precedentemente, con una stringa censurata.
+// .join(), transforma l'array in una stringa
+text = textTrim.join();
+
+//Restituzione dell'array con le parole sostituite e censurate;
+console.log(text);
